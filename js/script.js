83329c5d4c8c1164e0117908b9b3bc50957b107d
Paulo Ziembick <pauloziembick@gmail.com>
2024-07-30 02:04:55 -0300

    responsiveness

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll("nav ul li a");
-  
-    for (const link of links) {
-      link.addEventListener("click", smoothScroll);
-    }
-  
-    function smoothScroll(event) {
-      event.preventDefault();
-      const targetId = this.getAttribute("href").substring(1);
-      const targetSection = document.getElementById(targetId);
-  
-      window.scrollTo({
-        top: targetSection.offsetTop,
-        behavior: "smooth"
-      });
-    }
-  });
+  const links = document.querySelectorAll("nav ul li a");
+
+  for (const link of links) {
+    link.addEventListener("click", smoothScroll);
+  }
+
+  function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetSection.offsetTop,
+      behavior: "smooth",
+    });
+  }
+
+});
+
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu')
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active')
+    offScreenMenu.classList.toggle('active')
+})
